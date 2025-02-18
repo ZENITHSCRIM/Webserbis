b90ee3ae49c18eb9c93fc064eb1cbc9027132703
Vigyan <124452929+aerser@users.noreply.github.com>
2025-02-19 02:32:20 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ const port = 3000;
 app.use(express.json());
 
 let users = [
-    // Sample data
     { username: 'user1', password: 'password1', icon: '', posts: ['Hello World!'] },
     { username: 'user2', password: 'password2', icon: '', posts: ['My first post!'] }
 ];
 
-// Endpoint to get user profile
+// ユーザープロフィールを取得するエンドポイント
 app.get('/user/:username', (req, res) => {
     const username = req.params.username;
     const user = users.find(user => user.username === username);
@@ -21,7 +20,7 @@ app.get('/user/:username', (req, res) => {
     }
 });
 
-// Endpoint to get user posts
+// ユーザーポストを取得するエンドポイント
 app.get('/user/:username/posts', (req, res) => {
     const username = req.params.username;
     const user = users.find(user => user.username === username);
@@ -29,6 +28,19 @@ app.get('/user/:username/posts', (req, res) => {
         res.json(user.posts);
     } else {
         res.status(404).send('User not found');
+    }
+});
+
+// 新しい投稿を作成するエンドポイント
+app.post('/user/:username/posts', (req, res) => {
+    const username = req.params.username;
+    const postContent = req.body.content;
+    const user = users.find(user => user.username === username);
+    if (user && postContent) {
+        user.posts.push(postContent);
+        res.status(201).send('Post created');
+    } else {
+        res.status(400).send('Bad request');
     }
 });
 
